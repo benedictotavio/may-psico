273d27db1348f5c4fe654ac8f72684ac9a077bf0
Otavio Benedicto <otavio.benedicto@HBR.LOCAL>
2023-05-09 10:33:12 -0300

    Build the Homepage with list services

---
+++ src/components/layout/ListGroup.tsx
@@ -0,0 +1,29 @@
+const ListGroup = () => {
+  return (
+    <ol class="list-group list-group-numbered">
+      <li class="list-group-item d-flex justify-content-between align-items-start">
+        <div class="ms-2 me-auto">
+          <div class="fw-bold">Subheading</div>
+          Cras justo odio
+        </div>
+        <span class="badge bg-primary rounded-pill">14</span>
+      </li>
+      <li class="list-group-item d-flex justify-content-between align-items-start">
+        <div class="ms-2 me-auto">
+          <div class="fw-bold">Subheading</div>
+          Cras justo odio
+        </div>
+        <span class="badge bg-primary rounded-pill">14</span>
+      </li>
+      <li class="list-group-item d-flex justify-content-between align-items-start">
+        <div class="ms-2 me-auto">
+          <div class="fw-bold">Subheading</div>
+          Cras justo odio
+        </div>
+        <span class="badge bg-primary rounded-pill">14</span>
+      </li>
+    </ol>
+  );
+};
+
+export default ListGroup;
--- src/components/view/Homeview.tsx
+++ src/components/view/Homeview.tsx
@@ -1,19 +1,26 @@
 import ButtonWPP from "../buttons/ButtonWPP";
+import ListGroup from "../layout/ListGroup";
 
 const Homeview = () => {
   return (
-    <article>
-      <div className="d-flex flex-row p-md-5">
-        <div>
-          <h4>ATENDIMENTO ON-LINE DE PSICOLOGIA CLÍNICA</h4>
-          <p>PARA ADOLESCENTES, ADULTOS E FAMÍLIAS.</p>
-          <p>
-            Fale diretamente comigo, tire suas dúvidas e agende a sua sessão!
-          </p>
-          <ButtonWPP />
+    <>
+      <section>
+        <div className="d-flex flex-row p-md-5">
+          <div>
+            <h4>ATENDIMENTO ON-LINE DE PSICOLOGIA CLÍNICA</h4>
+            <p>PARA ADOLESCENTES, ADULTOS E FAMÍLIAS.</p>
+            <p>
+              Fale diretamente comigo, tire suas dúvidas e agende a sua sessão!
+            </p>
+            <ButtonWPP />
+          </div>
         </div>
-      </div>
-    </article>
+      </section>
+      <section className="text-center">
+        <ListGroup />
+        <ButtonWPP />
+      </section>
+    </>
   );
 };
 
